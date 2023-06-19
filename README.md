fb0ce99202d6054932d88f29540595bb99cc3395
vpn-rjalil68 <136805734+vpn-rjalil68@users.noreply.github.com>
2023-06-19 16:07:17 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # vpnscript
+VPN Panel with v2ray supported
+
+wget https://raw.githubusercontent.com/vpn-rjalil68/vpnscript/main/setup.sh && chmod +x setup.sh && ./setup.sh
---
+++ about.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+clear
+echo -e "================================================="
+echo -e "#       Premium Auto Script By KEK BURUK        #"
+echo -e "#-----------------------------------------------#"
+echo -e "# For Debian 9 & Debian 10 64 bit               #"
+echo -e "# For Ubuntu 18.04 & Ubuntu 20.04 64 bit        #"
+echo -e "# For VPS with KVM and VMWare virtualization    #"
+echo -e "# VPN-BUDAKGERIK PREMIUM                        #"
+echo -e "#-----------------------------------------------#"
+echo -e "# Thanks To                                     #"
+echo -e "#-----------------------------------------------#"
+echo -e "# ALLAH SWT                                     #"
+echo -e "# vpn-budakgerik@gmail.com                      #"
+echo -e "# My Family                                     #"
+echo -e "# Google                                        #"
+echo -e "# And also for All my friend                    #"
+echo -e "#                                               #"
+echo -e "#                                               #"
+echo -e "#-----------------------------------------------#"
+echo -e "================================================="
